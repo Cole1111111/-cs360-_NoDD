9f33ab4eb25568a9155102ce1782c7eeda839eb3
Rory Arnone-Wheat <rfarnone@outlook.com>
2024-04-16 13:57:18 -0700

    Rory AW cleared up errors

--- nbproject/Student_Interface.php
+++ nbproject/Student_Interface.php
@@ -1,13 +1,9 @@
 <!doctype html>
-<link rel="stylesheet" href="style.css">
-
 <html>
     <head>
         <title>Student Interface</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
         
         <style>
             table {
